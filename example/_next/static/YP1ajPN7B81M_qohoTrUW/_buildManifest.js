27b5c076928d405dcd85c48b449cead6ac1573d8
Maxwell Fortney <maxwell@paella.dev>
2021-12-31 15:07:12 -0500

    Updates

---
+++ example/_next/static/YP1ajPN7B81M_qohoTrUW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cec8eae632d88c5d582f.js"],"/_error":["static/chunks/pages/_error-da6cd6ef59fb7a60e2d4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
